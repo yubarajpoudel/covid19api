8fee424230f8ea62391c014a63b91afc96de0435
yubaraj poudel <yubarajpoudel708@gmail.com>
2020-03-26 19:13:42 +0545

    data scrapping of nepal press release done

--- scrapper.js
+++ scrapper.js
@@ -6,25 +6,65 @@ const publishedDate = new Set();
 const title = new Set();
 const size = new Set();
 const download = new Set();
+const data = [];
+// const fetchData = async () => {
+  
+// };
+const getResults = () => {
+  const resultKeys = ["sn", "published_date", "title", "size", "download_url"];
+  console.log("fetching data");
+   axios.get(siteUrl)
+      .then(function (response) {
+        // handle success
+        // console.log(response.data);
+        const $ = cheerio.load(response.data);
+        $('#health-emergency tr').each((index, element) => {
+          var j = 0;
+          var objectResponse = {};
+          $(element).find('td').each((index, element) => {
+            console.log($(element));
+            if(j == 4) {
+              objectResponse[resultKeys[j]] = $(element).find('a').attr('href');
+            } else {
+              objectResponse[resultKeys[j]] = $(element).text();
+            }
+            
+            j++;
+          });
+          console.log(objectResponse);
+          data.push(objectResponse);
+        });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+        console.log(data);
+        return {
+            // positions: [...positions].sort(),
+            // tags: [...tags].sort(),
+            // locations: [...locations].sort(),
+            // categories: [...categories].sort(),
+            // siteName,
+            test: "working"
+        };
+        console.log("fetch completed");
+      });
+  }
 
-
-const fetchData = async () => {
-  const result = await axios.get(siteUrl);
-  return cheerio.load(result.data);
-};
-
-const getResults = async () => {
-  const $ = await fetchData();
-
+ 
   // siteName = $('.table > .table-hover > .fixed_header > .m-0').text();
   // $(".tags .tag").each((index, element) => {
   //   tags.add($(element).text());
-  // });
-  $(".table .table-hover .fixed_header .m-0 tbody .small").each((index, element) => {
-    console.log($(element).text());
-   // publishedDate.add($(element).text());
+  // // });
+  // $("#health-emergency .small").each((index, element) => {
+  //   console.log($(element).text());
+  //   respFroomServer = $(element).text();
+  //  // publishedDate.add($(element).text());
 
-  });
+  // });
  //  $("div.nav p").each((index, element) => {
  //   categories.add($(element).text());
  //  });
@@ -34,13 +74,5 @@ const getResults = async () => {
  //  positions.add($(element).text());
  // });
 //Convert to an array so that we can sort the results.
-return {
-  positions: [...positions].sort(),
-  tags: [...tags].sort(),
-  locations: [...locations].sort(),
-  categories: [...categories].sort(),
-  siteName,
- };
-};
 
 module.exports = getResults;
