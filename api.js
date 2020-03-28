bd6f19c04e770ec8a529922176b9fd6eaa957619
yubaraj poudel <yubarajpoudel708@gmail.com>
2020-03-28 21:27:59 +0545

    cors whitelisted for all domains

--- api.js
+++ api.js
@@ -2,17 +2,30 @@ var createError = require('http-errors');
 const express = require('express')
 var indexRouter = require('./routes/index');
 const app = express();
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const responseTime = require('response-time')
 
+const port_redis = process.env.PORT || 6379;
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
+// use response-time as a middleware
+app.use(responseTime());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/', indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
