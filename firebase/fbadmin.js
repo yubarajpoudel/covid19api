5b0499748f64ebb69563cff31dcf4106c57f3ac7
Yubaraj Poudel <yubarajpoudel708@gmail.com>
2022-01-03 07:48:21 +0000

    firebase cred updated

--- firebase/fbadmin.js
+++ firebase/fbadmin.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
 
-var serviceAccount = require("../covi19-33867-firebase-adminsdk-196bn-90c32ade6c.json");
+var serviceAccount = require("../covid19-b3a31-firebase-adminsdk-mlhi5-2406946e94.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://covi19-33867.firebaseio.com"
+  databaseURL: "https://covid19-b3a31.firebaseio.com"
 });
 
 module.exports = admin;
